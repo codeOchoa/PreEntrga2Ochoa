dd7cb357f4830d31664cf1898b8e23fba6944437
codeOchoa <pablo.m.ochoa@gmail.com>
2024-07-15 23:47:40 -0300

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,17 @@
-const buscar__id = document.getElementById('buscar__id');
-const search = document.getElementById('search');
-const cancel = document.getElementById('cancel');
+document.addEventListener('scroll', function () {
+    var navBar = document.getElementById('nav-bar');
+    var specificDiv = document.getElementById('specific-div');
+    var specificDivTop = specificDiv.getBoundingClientRect().top;
 
-search.addEventListener('click', () => {
-    buscar__id.focus();
-});
-buscar__id.addEventListener('input', () => {
-    if (buscar__id.value.length > 0) {
-        cancel.style.display = 'block';
+    if (specificDivTop <= 0) {
+      navBar.style.top = '-100px'; // Ajusta este valor según la altura de tu nav-bar
     } else {
-        cancel.style.display = 'none';
+      navBar.style.top = '0';
     }
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    fetch("/includes/footer.html")
+        .then(response => response.text())
+        .then(data => document.getElementById("footer").innerHTML = data);
 });
-cancel.addEventListener('click', () => {
-    buscar__id.value = '';
-    cancel.style.display = 'none';
-});
-function incrementar() {
-    var input = document.getElementById('numero');
-    var numero = parseInt(input.value, 10);
-    numero = isNaN(numero) ? 0 : numero;
-    numero++;
-    input.value = numero;
-}
-function decrementar() {
-    var input = document.getElementById('numero');
-    var numero = parseInt(input.value, 10);
-    numero = isNaN(numero) ? 0 : numero;
-    numero--;
-    input.value = numero;
-}
-
-
-
-
-
-
-    // Cuando se carga la página
-    window.onload = function() {
-        var prevScrollpos = window.pageYOffset;
-        window.onscroll = function() {
-            var currentScrollPos = window.pageYOffset;
-            if (prevScrollpos > currentScrollPos) {
-                document.getElementById("navbar").style.top = "0";
-            } else {
-                document.getElementById("navbar").style.top = "-50px"; // Altura de la barra de navegación
-            }
-            prevScrollpos = currentScrollPos;
-        }
-    }
